dea01ca38bac9ad0cb6f8ce992ff8b3adcb59ea7
Captain Awesome <maxmilianhalim@Captain-Awesome-2.local>
2023-07-22 19:50:32 +0700

    test

---
+++ sign-in.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('signinForm');
+
+  //event listener - function
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('floatingInput').value;
+    const password = document.getElementById('floatingPassword').value;
+
+    //check if field is empty
+    if (email.trim() === '' || password.trim() === '') {
+      alert('Please fill in both fields.'); //error message
+      document.getElementById('floatingInput').value = ''; //clears the entries
+      document.getElementById('floatingPassword').value = '';
+      
+    } else {
+
+      window.location.href = '/Users/captainawesome/Desktop/GARUDAHACKS/index.html';
+    }
+  });
+});
